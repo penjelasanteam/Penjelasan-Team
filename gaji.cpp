9ceb31c9295d2fc77cbbc0f0beb05eaa9f6387af
penjelasanteam <49904504+penjelasanteam@users.noreply.github.com>
2019-05-21 20:31:35 +0700

    Add files via upload

---
+++ gaji.cpp
@@ -0,0 +1,171 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+using namespace std;
+
+void login(),menu(),gaji();
+int a,b,c,d,e,f,g,h,i,golongan;
+    char nama[20];
+main()
+{
+    login();
+    system("cls");
+    getch();
+
+}
+
+void menu()
+{   balik:
+    int pilih;
+    cout <<"                          ------Menu------" << endl;
+    cout <<"        =================================================="<<endl;
+    cout <<"        ||  Golongan 1 = jumlah jam kerja dikali 60000    ||"<<endl;
+    cout <<"        ||  Golongan 2 = jumlah jam kerja dikali 70000    ||"<<endl;
+    cout <<"        ||  Golongan 3 = jumlah jam kerja dikali 80000    ||"<<endl;
+    cout <<"        ||  Golongan 4 = jumlah jam kerja dikali 90000    ||"<< endl;
+    cout <<"        ||  Lembur jumlah jam kerja dikali 40000          ||"<<endl;
+    cout <<"        ==================================================";
+
+
+    cout << "\n\n           1. Cek Gaji  " << endl ;
+    cout << "           2. Keluar    " << endl;
+    cout << "           Masukan Pilihan Anda : ";
+    cin >> pilih;
+
+
+    if (pilih == 1)
+    {
+        system("cls");
+        gaji();
+    }
+    else if (pilih==2)
+    {
+        exit(EXIT_FAILURE);
+    }
+    else
+        { cout<<"Pilihan anda salah"<<endl;
+         goto balik;
+        }
+}
+void login()
+{
+    int i;
+char ch;
+cout << "============================\n";
+cout << "Selamat Datang di Form Login \n";
+cout << "============================\n";
+    for (i=1; i<=3; i++) {
+       string user = "";
+       string pass = "";
+       cout << "Username : "; cin >> user;
+       cout << "Password : ";
+            ch = _getch();
+            while(ch != 13){//character 13 is enter
+            pass.push_back(ch);
+            cout << '*';
+            ch = _getch();
+            }
+
+       if (user == "ara" && pass == "ara")
+        {
+         system("cls");
+         menu();
+        }
+       else
+        {
+         cout << "\n\nMaaf Username & Password anda masukan salah.\n\n";
+        }
+
+   }
+   while (i <= 3);
+
+   cout << "Anda telah 3x memasukan Username & Password yang salah.\n";
+   cout << "Mohon maaf akun anda kami blokir untuk sementara. \n";
+   cout << "Silahkan hubungi kami melalui e-mail support@email.com, Terima Kasih..";
+   getch();
+}
+
+void gaji()
+{
+    int a,b,c,d,e,f,g,h,i,golongan;
+    char nama[20];
+    cout<<"\n   Nama Karyawan         : ";cin>>nama;
+    cout<<"\n   Golongan [1/2/3/4]    : ";cin>>golongan;
+    cout<<"\n   Jumlah Jam Kerja      : ";cin>>a;
+    cout<<endl;
+
+    if (a>48)
+    {
+        b=a-48;
+        c=48;
+    }
+    else
+    {
+        b=0;
+        c=a;
+    }
+
+    d=b*40000;
+
+    if (golongan==1)
+    {
+        f=c*60000;
+    }
+    else
+    if (golongan==2)
+    {
+        f=c*70000;
+    }
+    else
+    if (golongan==3)
+    {
+        f=c*80000;
+    }
+    else
+    if(golongan==4)
+    {
+        f=c*90000;
+    }
+    else
+    {
+        cout<<"Golongan Tidak ditemukan"<<endl;
+        cout<<endl;
+    }
+    //upah karyawan
+    e=f+d;
+
+    cout<<"   ====================================";
+    cout<<"\n   Hasil Penghitungan Gaji Karyawan";
+    cout<<"\n   ====================================";
+    cout<<endl;
+    cout << "   ======================================\n";
+    cout<<"   || Nama Karyawan        : "<<nama<<endl;
+    cout<<"   || Upah Kerja           : Rp "<<f<<endl;
+    cout<<"   || Lembur               : Rp "<<d<<endl;
+    cout<<"   || Upah Keselurahan     : Rp "<<e<<endl;
+    cout << "   ======================================\n\n";
+
+    char yt;
+    kembali :
+    cout  << "Apakah Ingin Cek Lagi (Y/N) : ";
+    cin >> yt;
+    if (yt == 'Y' | yt=='y')
+    {
+        system("cls");
+        gaji();
+    }
+    else if(yt == 'N' | yt=='n')
+    {
+        system("cls");
+        menu();
+    }
+    else
+    {
+        cout << "Pilihan Yang Anda Masukan Salah" << endl;
+        goto kembali;
+    }
+}
+
+
+
+
